46fef060f9d9798dab9c9ef1f48442ae11a463e1
Konstantin Zhdanov <k.zhdan0v@yandex.ru>
2019-05-07 00:07:16 +0300

    Add method 'saveChanges' to save the tables into the network.
    Copy all the weights and biases from this tab's tables to
    the associated network between the layers this tab is associated with.

--- src/main/java/trainerapp/gui/controller/ViewNNTabController.java
+++ src/main/java/trainerapp/gui/controller/ViewNNTabController.java
@@ -36,28 +36,81 @@ public class ViewNNTabController implements Initializable {
     @FXML
     private Label prevLayerNeuronLabel;
     
+    private NeuralNetwork network;
+    
+    private ObservableList<Double> layerBiases;
+    
+    private ObservableList<ObservableList<Double>> layerWeights;
+    
+    private int curLayerNum;
+    
+    private int curLayerSize;
+    
+    private int prevLayerSize;
+    
+    public ObservableList<ObservableList<Double>> getWeights() {
+        return layerWeights;
+    }
+    
+    public ObservableList<Double> getBiases() {
+        return layerBiases;
+    }
+    
     public void setUp(NeuralNetwork nn, int curLayerNum) {
-        int curLayerSize;
-        int prevLayerSize;
+        if (nn == null) {
+            throw new NullPointerException("Network cannot be null");
+        }
+        if (curLayerNum < 0) {
+            throw new IllegalArgumentException("Index of layer cannot be negative");
+        }
+        network = nn;
+        this.curLayerNum = curLayerNum;
+        initLayerInfoData();
+
+        // Set weights and biases
+        layerBiases = FXCollections.observableList(
+            new ArrayList<>(curLayerSize));
+        for (int toNeuron = 0; toNeuron < curLayerSize; toNeuron++) {
+            weightsTableViewFacade.addColumn("Neuron " + (toNeuron + 1));
+            biasesTableViewFacade.addColumn("Neuron " + (toNeuron + 1));
+            layerBiases.add(nn.getBias(curLayerNum, toNeuron));
+        }
+        biasesTableViewFacade.getItems().add(layerBiases);
+        
+        layerWeights = FXCollections.observableList(
+                new ArrayList<>(prevLayerSize));
+        for (int fromNeuron = 0; fromNeuron < prevLayerSize; fromNeuron++) {
+            ObservableList<Double> weightsFromNeuron = 
+                    FXCollections.observableList(new ArrayList<>(
+                            curLayerSize));
+            for (int toNeuron = 0; toNeuron < curLayerSize; toNeuron++) {
+                 weightsFromNeuron.add(nn.getWeight(curLayerNum, fromNeuron, toNeuron));
+            }
+            layerWeights.add(weightsFromNeuron);
+        }
+        weightsTableViewFacade.setItems(layerWeights);
+    }
+    
+    private void initLayerInfoData() {
         String curLayerStr;
         String prevLayerStr;
         if (curLayerNum == 0) {
             // first hidden layer
-            curLayerSize = nn.getHiddenLayerSize(curLayerNum);
-            prevLayerSize = nn.getNumberInputs();
+            curLayerSize = network.getHiddenLayerSize(curLayerNum);
+            prevLayerSize = network.getNumberInputs();
             curLayerStr = "Layer 1";
             prevLayerStr = "Input layer";
         }
-        else if (curLayerNum == nn.getNumberHiddenLayers()) {
+        else if (curLayerNum == network.getNumberHiddenLayers()) {
             // output layer
-            curLayerSize = nn.getNumberOutputs();
-            prevLayerSize = nn.getHiddenLayerSize(curLayerNum - 1);
+            curLayerSize = network.getNumberOutputs();
+            prevLayerSize = network.getHiddenLayerSize(curLayerNum - 1);
             curLayerStr = "Output layer";
             prevLayerStr = "Layer " + (curLayerNum);
         }
         else {
-            curLayerSize = nn.getHiddenLayerSize(curLayerNum);
-            prevLayerSize = nn.getHiddenLayerSize(curLayerNum - 1);
+            curLayerSize = network.getHiddenLayerSize(curLayerNum);
+            prevLayerSize = network.getHiddenLayerSize(curLayerNum - 1);
             curLayerStr = "Layer " + (curLayerNum + 1);
             prevLayerStr = "Layer " + (curLayerNum);
         }
@@ -66,24 +119,21 @@ public class ViewNNTabController implements Initializable {
         curLayerNeuronLabel.setText(curLayerStr + "'s neurons");
         curLayerBiasesLabel.setText(curLayerStr + "'s biases");
         prevLayerNeuronLabel.setText(prevLayerStr + "'s neurons");
-             
-        // Set weights and biases
-        ObservableList<Double> biases = FXCollections.observableList(
-            new ArrayList<>(curLayerSize));
-        for (int toNeuron = 0; toNeuron < curLayerSize; toNeuron++) {
-            weightsTableViewFacade.addColumn("Neuron " + (toNeuron + 1));
-            biasesTableViewFacade.addColumn("Neuron " + (toNeuron + 1));
-            biases.add(nn.getBias(curLayerNum, toNeuron));
+    }
+    
+    public void saveChanges() {
+        if (network == null) {
+            throw new IllegalStateException("This tab hasn't been set up");
         }
-        biasesTableViewFacade.getItems().add(biases);
+        for (int toNeuron = 0; toNeuron < curLayerSize; toNeuron++) {
+            network.setBias(curLayerNum, toNeuron, layerBiases.get(toNeuron));
+        }
+
         for (int fromNeuron = 0; fromNeuron < prevLayerSize; fromNeuron++) {
-            ObservableList<Double> weightsFromNeuron = 
-                    FXCollections.observableList(new ArrayList<>(
-                            curLayerSize));
             for (int toNeuron = 0; toNeuron < curLayerSize; toNeuron++) {
-                 weightsFromNeuron.add(nn.getWeight(curLayerNum, fromNeuron, toNeuron));
+                 network.setWeight(curLayerNum, fromNeuron, toNeuron, 
+                         layerWeights.get(fromNeuron).get(toNeuron));
             }
-            weightsTableViewFacade.getItems().add(weightsFromNeuron);
         }
     }
     
