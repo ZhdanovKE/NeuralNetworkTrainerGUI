d13692533f04f7448cb7db7c89376b7fe7b5a2c2
Konstantin Zhdanov <k.zhdan0v@yandex.ru>
2019-05-07 19:39:26 +0300

    Add min size constraints to the Main Window.

--- src/main/java/trainerapp/gui/MainApp.java
+++ src/main/java/trainerapp/gui/MainApp.java
@@ -16,12 +16,27 @@ public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainWindow.fxml"));
-        
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root, root.prefWidth(-1), root.prefHeight(-1));
         scene.getStylesheets().add("/styles/Styles.css");
         
         stage.setTitle("Neural Network Trainer GUI");
         stage.setScene(scene);
+        stage.setOnShowing((event) -> {
+            stage.hide();
+        });
+        stage.setOnShown((event) -> {
+            double widthDelta = stage.getWidth() - stage.getScene().getWidth();
+            double heightDelta = stage.getHeight() - stage.getScene().getHeight();
+            if (widthDelta < 0) {
+                widthDelta = 0;
+            }
+            if (heightDelta < 0) {
+                heightDelta = 0;
+            }
+            stage.setMinWidth(stage.getScene().getRoot().minWidth(-1) + widthDelta);
+            stage.setMinHeight(stage.getScene().getRoot().minHeight(-1) + heightDelta);
+            stage.show();
+        });
         stage.show();
     }
 
