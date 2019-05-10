50668f31ec6b4063f94b0b2c1311a5c4dc8515e0
Konstantin Zhdanov <k.zhdan0v@yandex.ru>
2019-05-10 22:25:05 +0300

    Add Javadoc

--- src/main/java/trainerapp/gui/repository/SamplesRepository.java
+++ src/main/java/trainerapp/gui/repository/SamplesRepository.java
@@ -7,7 +7,8 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 /**
- * Repository for storing neural network's samples, containing numbers.
+ * Repository for storing neural network's samples, containing numbers
+ * and an optional header and preserving the insertion order of elements.
  * @param <T> A numerical type that the samples hold.
  * @author Konstantin Zhdanov
  */
@@ -16,11 +17,19 @@ public class SamplesRepository<T extends Number> {
     
     private final ObservableList<ObservableList<T>> items;
     
+    /**
+     * Create an empty repository of samples.
+     */
     public SamplesRepository() {
         items = FXCollections.observableArrayList();
         header = new LinkedList<>();
     }
     
+    /**
+     * Add a sample into this repository.
+     * @param sample {@code ObservableList} of numerical values to be add as a sample.
+     * @throws NullPointerException if {@code sample} is null.
+     */
     public void add(ObservableList<T> sample) {
         if (sample == null) {
             throw new NullPointerException("Sample cannot be null");
@@ -28,14 +37,35 @@ public class SamplesRepository<T extends Number> {
         items.add(sample);
     }
     
+    /**
+     * Remove the sample with index {@code idx} from this repository.
+     * @param idx Index of sample to be removed from this repository.
+     * @throws IndexOutOfBoundsException if {@code idx} is out of range.
+     */
     public void remove(int idx) {
         items.remove(idx);
     }
     
+    /**
+     * Set the {@code header} list as the headers (names) for the samples' variables.
+     * @param header {@code List} of {@String} values of header for every variable
+     * in each sample.
+     * @throws NullPointerException if {@code header} is null.
+     */
     public void setHeader(List<String> header) {
         this.header.addAll(header);
     }
     
+    /**
+     * Get the header list associated with the samples loaded in this
+     * repository. If this repository is holding any samples and the header
+     * hasn't been set explicitly, the returned list contains the default header
+     * values: "Var 1", "Var 2", ..., "Var n". 
+     * If no samples have ever been loaded in this repository and the header
+     * hasn't been set explicitly, the returned list will be empty.
+     * @return {@code List} of {@code String} header (names) values for the 
+     * samples' variables. The returned list is unmodifiable.
+     */
     public List<String> getHeader() {
         if (header.isEmpty() && !items.isEmpty()) {
             createDefaultHeader();
@@ -51,18 +81,42 @@ public class SamplesRepository<T extends Number> {
         }
     }
     
+    /**
+     * Get the sample with index {@code idx} from this repository.
+     * @param idx The index of the sample to be returned.
+     * @return {@code ObservableList} of numerical values that this repository holds
+     * under the index {@code idx}. Any changes to this list will change the
+     * object this repository holds.
+     * @throws IllegalArgumentException if {@code idx} is out of range.
+     */
     public ObservableList<T> getSample(int idx) {
         return items.get(idx);
     }
     
+    /**
+     * Get all samples this repository holds.
+     * @return {@code ObservableList} of all samples this repository holds. Any changes
+     * to this list will change the samples this repository holds.
+     */
     public ObservableList<ObservableList<T>> getAll() {
         return items;
     }
     
+    /**
+     * Number of samples this repository holds.
+     * @return {@code int} value representing the number of samples in this
+     * repository.
+     */
     public int size() {
         return items.size();
     }
     
+    /**
+     * Size of the samples this repository holds. If the repository is empty, 
+     * the size is zero.
+     * @return {@code int} value holding the size of the samples in this 
+     * repository. If the repository is empty, 0 is returned.
+     */
     public int sampleSize() {
         if (items.isEmpty()) {
             return 0;
@@ -70,6 +124,11 @@ public class SamplesRepository<T extends Number> {
         return items.get(0).size();
     }
     
+    /**
+     * Whether this repository is empty.
+     * @return {@code boolean} value representing if this repository holds 
+     * any samples or not.
+     */
     public boolean isEmpty() {
         return items.isEmpty();
     }
